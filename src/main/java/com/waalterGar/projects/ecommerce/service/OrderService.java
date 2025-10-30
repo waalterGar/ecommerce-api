3b0576e0412676a28c776dc9ff2cf7007c281879
waalterGar <walter.garciagar@gmail.com>
2025-10-30 11:38:51 +0100

    feat(orders): pageable list endpoint with validated sorting + default ordering

--- src/main/java/com/waalterGar/projects/ecommerce/service/OrderService.java
+++ src/main/java/com/waalterGar/projects/ecommerce/service/OrderService.java
@@ -3,7 +3,10 @@ package com.waalterGar.projects.ecommerce.service;
 
 import com.waalterGar.projects.ecommerce.Dto.OrderDto;
 import com.waalterGar.projects.ecommerce.Dto.PayOrderRequestDto;
+import com.waalterGar.projects.ecommerce.Dto.ProductDto;
 import com.waalterGar.projects.ecommerce.Dto.createOrderDto;
+import com.waalterGar.projects.ecommerce.api.pagination.PageEnvelope;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -13,4 +16,5 @@ public interface OrderService {
     OrderDto getOrderByExternalId(String orderNumber);
     OrderDto pay(String externalId, PayOrderRequestDto dto);
     OrderDto cancelOrder(String externalId);
+    PageEnvelope<OrderDto> list(Pageable pageable);
 }
